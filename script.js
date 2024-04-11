afe0ca1b6c993bf2a8f63aba3004b31a7efb7fce
pallavi thakur <thakurpallavi560@gmail.com>
2024-04-11 15:14:31 +0530

    function added to the java

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 let next = document.querySelector('.next')
 let prev = document.querySelector('.prev')
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
