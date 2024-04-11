332e4f4a4d7f8666a7778954a5adf8477595173b
neha <nehamalik1131@gmail.com>
2024-04-11 15:27:10 +0530

    Revert "function added to the java"
    This reverts commit afe0ca1b6c993bf2a8f63aba3004b31a7efb7fce.

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 let next = document.querySelector('.next')
 let prev = document.querySelector('.prev')
-next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-})
