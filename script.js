870c2e418b019ee2f04735d74891f332bf8867a3
neha <nehamalik1131@gmail.com>
2024-04-11 15:51:12 +0530

    previous function added in java

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ next.addEventListener('click', function(){
     document.querySelector('.slide').appendChild(items[0])
 })
 
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
