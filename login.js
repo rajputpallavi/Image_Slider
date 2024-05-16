629934e379bd87cba9bf1fce064c190b4b514384
pallavi thakur <thakurpallavi560@gmail.com>
2024-05-16 22:16:15 +0530

    formatting the code

--- login.js
+++ login.js
@@ -6,12 +6,13 @@ function login() {
         alert('Please enter a valid email address.');
         return;
     }
+    
     if (password.length < 8) {
         console.log('Password must be at least 8 characters long.');
         alert('Password must be at least 8 characters long.');
         return;
     }
-    
+
     console.log('Email and password entered successfully.');
     window.location.href = 'another-page.html';
 }
